f52316045de2930eb3f81ba699940738ade3c072
HaruYuki-Ar1ta <charleCampbell189@gmail.com>
2022-05-18 17:47:04 -0400

    Done

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.*;
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+
+       ArrayList<Double> arrayList = new ArrayList<Double>();
+        for(double i = 0; i> arrayList.size();i++){
+            arrayList.add(i);
+            arrayList.remove(2.0);
+            arrayList.remove(4.0);
+            arrayList.set( 2,  2.0);
+    }
+
+    }
+}
+
